f9c56191250e615bf75ad42056f087a6846b8595
Priyanshi  Sharma <25120cn044@glbitm.ac.in>
2026-03-19 22:06:41 +0530

    Create 18th.c

---
+++ 18th.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int num, reverse = 0, remainder;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    while (num != 0)
+      {
+        remainder = num % 10;        // get last digit
+        reverse = reverse * 10 + remainder; // build reverse
+        num = num / 10;             // remove last digit
+    }
+    printf("Reversed number = %d", reverse);
+    return 0;
+}
