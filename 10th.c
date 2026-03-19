29050b42c8ae0fb185d88729cafab1d7e3bd07e8
Priyanshi  Sharma <25120cn044@glbitm.ac.in>
2026-03-19 21:50:36 +0530

    Create 10th.c

---
+++ 10th.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    float m1, m2, m3, m4, m5, total, percentage;
+    printf("Enter marks of 5 subjects:\n");
+    scanf("%f %f %f %f %f", &m1, &m2, &m3, &m4, &m5);
+    total = m1 + m2 + m3 + m4 + m5;
+    percentage = total / 5;
+    printf("Percentage = %.2f%%\n", percentage);
+    if (percentage >= 90 && percentage <= 100)
+    {
+        printf("Grade: A\n");
+    } 
+    else if (percentage >= 80)
+    {
+        printf("Grade: B\n");
+    } 
+    else if (percentage >= 60)
+    {
+        printf("Grade: C\n");
+    } 
+    else
+    {
+        printf("Grade: D\n");
+    }
+
+    return 0;
+}
