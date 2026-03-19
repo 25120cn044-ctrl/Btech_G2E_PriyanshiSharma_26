6f0fc29bf72c07e43b539b8222ffcab489f6a4eb
Priyanshi  Sharma <25120cn044@glbitm.ac.in>
2026-03-19 21:54:31 +0530

    Create 12th.c

---
+++ 12th.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int num, i;
+    long long factorial = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num < 0) 
+    {
+        printf("Factorial of a negative number is not defined.");
+    } 
+    else
+    {
+        for (i = 1; i <= num; i++) 
+        {
+            factorial *= i;
+        }
+        printf("Factorial = %lld", factorial);
+    }
+    return 0;
+}
